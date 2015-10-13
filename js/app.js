df5d80db6dbc80134707a6c5c60ba64c14657f21
vinit <vinitagarwal069@gmail.com>
2015-10-14 00:00:48 +0530

    Implemented to do list with login

--- js/app.js
+++ js/app.js
@@ -1,37 +1,44 @@
-var task = [];
-var show = document.querySelector('#showTask');
+(function(){ 
+    var task = [];
+    var show = document.querySelector('#showTask');
 
-window.addList = function(){
-    task.push(document.getElementById('input').value);
-    for(i = 0; i < task.length; i++){
-        var newList = "<a onclick = 'removeItem(" + i + ");'>" + task[i] + "</a><br>";
+    window.addList = function(){
+        var inp = document.getElementById('input').value;
+        if(document.getElementById('input').value === null || document.getElementById('input').value === ''){
+            alert('please enter a text in the field');
+        } else {
+        task.push(inp);
+        for(i = 0; i < task.length; i++){
+            var newList = "<a onclick = 'removeItem(" + i + ");'>" + task[i] + "</a><br>";
+        }
+        show.innerHTML += newList;
+        storeLocal();
+        var x = document.getElementById('input');
+        x.focus();}
+        }
+
+    window.removeItem = function(i){
+        task.splice(i,1);
+        var show = document.getElementById('showTask');
+        var newList = "";
+        for(i = 0; i < task.length; i++){
+            newList += "<a onclick = 'removeItem(" + i + ");'>" + task[i] + "</a><br>";
+        }
+        show.innerHTML = newList;
+        storeLocal();}
+
+    document.addEventListener('DOMContentLoaded', retrieveLocal, false);
+
+    function storeLocal(){
+        //localStorage.clear();
+        localStorage.showTask = show.innerHTML;
+        localStorage["task"] = JSON.stringify(task);
     }
-    show.innerHTML += newList;
-    storeLocal();
-var x = document.getElementById('input');
-x.focus();}
 
-window.removeItem = function(i){
-    task.splice(i,1);
-    var show = document.getElementById('showTask');
-    var newList = "";
-    for(i = 0; i < task.length; i++){
-        newList += "<a onclick = 'removeItem(" + i + ");'>" + task[i] + "</a><br>";
+    function retrieveLocal(){
+        if(localStorage.showTask){
+            show.innerHTML = localStorage.showTask;
+            task = JSON.parse(localStorage["task"]);
+        }
     }
-    show.innerHTML = newList;
-    storeLocal();}
-
-document.addEventListener('DOMContentLoaded', retrieveLocal, false);
-
-function storeLocal(){
-    //localStorage.clear();}
-    localStorage.showTask = show.innerHTML;
-    localStorage["task"] = JSON.stringify(task);
-}
-
-function retrieveLocal(){
-    if(localStorage.showTask){
-        show.innerHTML = localStorage.showTask;
-        task = JSON.parse(localStorage["task"]);
-    }
-}
+})();
--- js/login.js
+++ js/login.js
@@ -1,11 +1,21 @@
-var name = 'vinitagarwal069@gmail.com';
-var pass = '12345678';
-var userName = document.getElementById('userName').value;
-var password = document.getElementById('passwrd').value;
-var form = document.querySelector('form');
+var name = "vinitagarwal069@gmail.com";
+var pass = "12345678";
+var user, pwd ;
 
-form.addEventListener('submit', function(ev){
-    if(userName == '' || password == '' || userName == null || password == null){
+function login(){
+    user = document.getElementById("input").value;
+    pwd = document.getElementById("passwrd").value;
+    if(user == '' || pwd == '' || user == null || pwd == null){
         alert('fields cannot be empty');
+    } else if(user == name && pwd == pass){
+        setTimeout(function(){
+            alert('loggedin successfully');
+            window.location.assign("http://127.0.0.1:60557/index.html");
+            }, 1000);
+    } else {
+        alert('please provide valid details');
+        document.getElementById("input").value ="";
+        document.getElementById("passwrd").value ="";
+        document.getElementById("input").focus();
     }
-});
+}
